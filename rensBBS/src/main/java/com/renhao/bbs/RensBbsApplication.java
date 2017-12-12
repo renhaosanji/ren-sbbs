0e0069f8644909116a06f91601b26e31e5766df7
RENHAO_LDCC <RENHAO_LDCC@RENHAO_LDCC-PC>
2017-12-12 18:19:21 +0900

    ss

---
+++ rensBBS/src/main/java/com/renhao/bbs/RensBbsApplication.java
@@ -0,0 +1,12 @@
+package com.renhao.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RensBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RensBbsApplication.class, args);
+	}
+}
