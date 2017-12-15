eb2c41ef95e63082a9f6ef35b38c5acb716cdad3
RENHAO_LDCC <RENHAO_LDCC@RENHAO_LDCC-PC>
2017-12-15 19:38:33 +0900

    kk

--- rensBBS/src/main/java/com/renhao/bbs/controller/BBSMainPageController.java
+++ rensBBS/src/main/java/com/renhao/bbs/controller/BBSMainPageController.java
@@ -1,15 +1,24 @@
 package com.renhao.bbs.controller;
 
+import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class BBSMainPageController {
 
-	@RequestMapping("/home")
-	private String returnToMainPage() {
-		System.out.println("dddddddddd");
+	@RequestMapping(value ="/home1")
+	public String returnToMainPage() {
+		System.out.println("mainPage request got");
 		
 		return "BBSmainPage";
 	}
+	
+	
+	@Test
+	public void foo(){
+		System.out.println("JunitTest!");
+	}
+	
 }
